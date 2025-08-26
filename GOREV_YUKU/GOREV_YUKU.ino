328d1332c3c3db2417ff2b5b579a1f2c53b00c18
Akif9748 <akif9748@gmail.com>
2025-08-27 00:39:27 +0300

    csv denemesi

--- GOREV_YUKU/GOREV_YUKU.ino
+++ GOREV_YUKU/GOREV_YUKU.ino
@@ -2,7 +2,7 @@
 #define GpsRX D9
 #define LoraTX D12
 
-#define LORA_INTERVAL 200
+#define HZ_5 200
 
 #define HEADER_BYTE 0xAA
 #define FOOTER_BYTE 0x55
@@ -12,45 +12,57 @@
 #include <HardwareSerial.h>
 #include <TinyGPS++.h>
 #include <Adafruit_BMP280.h>
+#include <SPIFFS.h>
 
 
 Adafruit_BMP280 BMP;
 TinyGPSPlus GPS;
-HardwareSerial LoraSerial(2);
-
-
-TaskHandle_t AnaAlgoritma;
-TaskHandle_t Communication;
-
+HardwareSerial ComboSerial(2);
+File LOG;
 
 void setup() {
   pinMode(BUZZER, OUTPUT);
   digitalWrite(BUZZER, HIGH);
 
+  // SPIFFS başlat
+  if (!SPIFFS.begin(true)) {
+    Serial.println("SPIFFS err");
+    while (1)
+      ;
+  }
+
+  // Dosyayı sürekli açık bırak
+  LOG = SPIFFS.open("/veri.csv", FILE_APPEND);
+  if (!LOG) {
+    Serial.println("veri.csv err");
+    while (1)
+      ;
+  }
+
   // LoRa ve GPS birlikte başlatma
-  LoraSerial.begin(9600, SERIAL_8N1, GpsRX, LoraTX);
+  ComboSerial.begin(9600, SERIAL_8N1, GpsRX, LoraTX);
 
   // BMP başlatma
   BMP.begin(0x76);  //veya 77 denenecek
 
-  xTaskCreatePinnedToCore(
-    Degiskenler,
-    "AnaAlgoritma",
-    10000,
-    NULL,
-    1,
-    &AnaAlgoritma,
-    0);
-  delay(100);
 
   xTaskCreatePinnedToCore(
     Haberlesme,
-    "Communication",
+    "Haberlesme",
     10000,
     NULL,
     1,
-    &Communication,
+    NULL,
     1);
+  delay(100);
+  xTaskCreatePinnedToCore(
+    Depolama,
+    "Depolama",
+    10000,
+    NULL,
+    1,
+    NULL,
+    0);
 
   delay(400);
 
@@ -69,7 +81,7 @@ float sicaklik, yogunluk, basinc;
 
 typedef struct __attribute__((packed)) {
   uint8_t header;  // 1 byte
-  
+
   float enlem;     // 4 byte
   float boylam;    // 4 byte
   float irtifa;    // 4 byte
@@ -77,35 +89,40 @@ typedef struct __attribute__((packed)) {
   float yogunluk;  // 4 byte (kg/m3)
   float sicaklik;  // 4 byte (°C)
 
-  uint8_t checksum;// 1 byte
-  uint8_t footer;  // 1 byte
+  uint8_t checksum;  // 1 byte
+  uint8_t footer;    // 1 byte
 
 } VeriPaketi;
 
 
-void Degiskenler(void* pvParameters) {
-  while (1) {
-    while (LoraSerial.available())
-      GPS.encode(LoraSerial.read());
-
-    if (GPS.location.isValid()) {
-      enlem = GPS.location.lat();
-      boylam = GPS.location.lng();
-    } 
-    if (GPS.altitude.isValid()) {
-      irtifa = GPS.altitude.meters();
-    } 
-
-    sicaklik = BMP.readTemperature();
-    basinc = BMP.readPressure();
-    yogunluk = basinc / (R * (sicaklik + 273.15));
-
-    vTaskDelay(LORA_INTERVAL / portTICK_PERIOD_MS);
+void Depolama(void* pvParameters) {
+  for (;;) {
+    if (LOG) {
+      LOG.printf("%.6f,%.6f,%.2f,%.2f,%.2f,%.2f\n",
+                     enlem, boylam, irtifa, basinc, yogunluk, sicaklik);
+      LOG.flush();
+    }
+    vTaskDelay(5000 / portTICK_PERIOD_MS);  // 5Hz
   }
 }
 
 void Haberlesme(void* pvParameters) {
   for (;;) {
+    while (ComboSerial.available())
+      GPS.encode(ComboSerial.read());
+
+    if (GPS.location.isValid()) {
+      enlem = GPS.location.lat();
+      boylam = GPS.location.lng();
+    }
+    if (GPS.altitude.isValid()) {
+      irtifa = GPS.altitude.meters();
+    }
+
+    sicaklik = BMP.readTemperature();
+    basinc = BMP.readPressure();
+    yogunluk = basinc / (R * (sicaklik + 273.15));
+
     VeriPaketi paket;
     paket.header = HEADER_BYTE;
     paket.enlem = enlem;
@@ -119,13 +136,13 @@ void Haberlesme(void* pvParameters) {
     uint8_t sum = 0;
     for (size_t i = 0; i < sizeof(VeriPaketi) - 2; ++i)
       sum += gonderilecekPaket[i];
-    
+
     paket.checksum = sum % 256;
 
     paket.footer = FOOTER_BYTE;
 
-    LoraSerial.write(gonderilecekPaket, sizeof(VeriPaketi));
-    vTaskDelay(LORA_INTERVAL / portTICK_PERIOD_MS);
+    ComboSerial.write(gonderilecekPaket, sizeof(VeriPaketi));
+    vTaskDelay(HZ_5 / portTICK_PERIOD_MS);
   }
 }
 
