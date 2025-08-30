bb2a61ab613f2c34081d393a690c50f0b85fbbbe
vakkas1234 <vakkasemreyildiz@gmail.com>
2025-08-30 10:37:43 +0300

    GÜZEL TÜRKİYEM E YOLCULUK . Güzelleştirme

--- GOREV_YUKU/Spiff.ino
+++ GOREV_YUKU/Spiff.ino
@@ -1,24 +1,29 @@
 #include <FS.h>
 #include <SPIFFS.h>
+bool kontrol = false;
 void setup(){
 Serial.begin(115200);
 if(!SPIFFS.begin()){
   Serial.print("Hata");
-  return;
-}
-File file = SPIFFS.open("/veri.csv","r");
-if(!file){
-  Serial.print("Bulunamadı CSV Dosyası");
-  return;
-}
-while(file.available()){
-  Serial.write(file.read());
+ return;
 }
 
+}
+void OKUMA(){
+  File file = SPIFFS.open("/veri.csv","r");
+  if(!file){
+    Serial.print("CSV Dosyası Bulunamadı");
+    return;
+  }
+  while(file.available()){
+    Serial.write(file.read());
+
+  }
 }
 void loop(){
-
-}
-void loop(){
-
+  if(!kontrol){
+    OKUMA();
+    kontrol = true;
+    return;
+  }
 }
